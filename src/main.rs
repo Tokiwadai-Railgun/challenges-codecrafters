a9982fed85beb8c572017ac4f85083716480fe28
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2024-11-13 17:33:22 +0100

    Initial github commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
-use flate2::read::{ZlibDecoder};
-#[allow(unused_imports)]
+use flate2::write::ZlibEncoder;
+use flate2::read::ZlibDecoder;
+use flate2::Compression;
 use std::io::prelude::*;
+use sha1::{Digest, Sha1};
+use hex;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,7 +20,7 @@ fn main() {
     } else if args[1] == "cat-file"{
         // We will try to recover the object : 9daeafb9864cf43055ae93beb0afd6c7d144bfa4
         // And : 250591f6d4523e8d78215e3ade01d5bd946ed33b
-
+        // test.txt = 2656faa96107f2c8028df08716cfe408e7ee3fed
         // First find the folder which contains the file
         let folder_name = &args[3][0..2];
         let file_name = &args[3][2..];
@@ -28,17 +29,46 @@ fn main() {
                 println!("Error fetching file")
             },
             Ok(content) => {
-                // Decompress to a usable string
-                let mut z = ZlibDecoder::new(&content[..]);
-                let mut s = String::new();
-                z.read_to_string(&mut s).expect("Error decoding data");
-                // Now filtering content to start after the first null char
-                let content = &s[s.find('\0').unwrap() + 1..];
-                print!("{content}")
-                // print!("Testing the print function")
+                // Decompress file
+                let unzipped_content = unzip_content(&content);
+                print!("{unzipped_content}")
             }
         }
+    }
+    else if args[1] == "hash-object"{
+        // testing on : 8a61407747a6b8cbdb840c42a660c220eb201e00
+
+        // First step : Read the file
+        let content = fs::read(format!("./{}", args[3])).expect("Error reading file data");
+
+        // Then create the file
+    
+        // Write Zipped content in it
+        let zipped_content = zip_content(&content).expect("Zip contained non UTF-8 characters");
     } else {
         println!("unknown command: {}", args[1])
     }
+}
+
+fn hash_content(content: &[u8]) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(content);
+    hex::encode(hasher.finalize())
+}
+
+fn unzip_content(content: &[u8]) -> String {
+    let mut d = ZlibDecoder::new(content);
+    let mut s = String::new();
+    d.read_to_string(&mut s).expect("Error dezipping data");
+    s
+}
+
+fn zip_content(content: &[u8]) -> Result<String, std::string::FromUtf8Error>{
+    // Also add the glob <size>\0 at the start of the file
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    let _ = e.write_all(b"blob ");
+    let _ = e.write_all(content.len().to_string().as_bytes());
+    let _ = e.write_all(b"\0");
+    let _ = e.write_all(content);
+    String::from_utf8(e.finish().expect("Failed compressing file"))
 }
